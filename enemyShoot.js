2ea9ada0b1e1c4a9cfbffd3bd882d2bde5a394ed
Sakshi <70653660+Sakshi887220@users.noreply.github.com>
2022-10-28 13:18:35 +0530

    Update enemyShoot.js

--- enemyShoot.js
+++ enemyShoot.js
@@ -7,25 +7,8 @@ AFRAME.registerComponent("enemy-bullets", {
         //get all enemies using className
         var els = document.querySelectorAll(".enemy");
 
-        for (var i = 0; i < els.length; i++) {           
-
-            //enemyBullet entity
-            var enemyBullet = document.createElement("a-entity");
-
-            enemyBullet.setAttribute("geometry", {
-                primitive: "sphere",
-                radius: 0.1,
-            });
-
-            enemyBullet.setAttribute("material", "color", "#282B29");
-
-            var position = els[i].getAttribute("position")
-
-            enemyBullet.setAttribute("position", {
-                x: position.x + 1.5,
-                y: position.y + 3.5,
-                z: position.z,
-            });
+        
+            
 
             var scene = document.querySelector("#scene");
             scene.appendChild(enemyBullet);
@@ -35,11 +18,7 @@ AFRAME.registerComponent("enemy-bullets", {
             var position2 = new THREE.Vector3();
 
             //shooting direction
-            var enemy = els[i].object3D;
-            var player = document.querySelector("#weapon").object3D;
-
-            player.getWorldPosition(position1);
-            enemy.getWorldPosition(position2);
+            
 
             //set the velocity and it's direction
             var direction = new THREE.Vector3();
@@ -58,26 +37,7 @@ AFRAME.registerComponent("enemy-bullets", {
 
             //collide event on enemy bullets
             enemyBullet.addEventListener("collide", function (e) {
-                if (e.detail.body.el.id === "weapon") {
-
-                    if (playerLife > 0) {
-                        playerLife -= 1;
-                        element.setAttribute("text", {
-                            value: playerLife
-                        });
-                    }
-                    if (playerLife <= 0) {
-                        //show text
-                        var txt = document.querySelector("#over");
-                        txt.setAttribute("visible", true);
-
-                        //remove tanks                        
-                        var tankEl = document.querySelectorAll(".enemy")
-
-                        for (var i = 0; i < tankEl.length; i++) {
-                            scene.removeChild(tankEl[i])
-
-                        }
+                
                     }
 
                 }
